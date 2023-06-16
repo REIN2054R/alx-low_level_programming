6317b9a5caf330ed14221267ad15cfcb121e2848
REIN2054R <grjo1213@gmail.com>
2023-06-16 07:47:49 -0700

    betty

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,19 +8,19 @@
 
 int main(void)
 {
-        int a;
-        int b;
+	int	a;
+	int	b;
 
-        a = 0;
-        while (a <= 99)
-        {
+	a = 0;
+	while (a <= 99)
+	{
 		b = a + 1;
 		while (b <= 99)
 		{
-			putchar(a  / 10 +48);
-			putchar(a % 10 +48);
+			putchar(a  / 10 + 48);
+			putchar(a % 10 + 48);
 			putchar(' ');
-			putchar(b/ 10 + 48);
+			putchar(b / 10 + 48);
 			putchar(b % 10 + 48);
 			if (a != 98 || b != 99)
 			{
@@ -30,7 +30,7 @@ int main(void)
 			b++;
 		}
 		a++;
-        }
-        putchar('\n');
-        return (0);
+	}
+	putchar('\n');
+	return (0);
 }
