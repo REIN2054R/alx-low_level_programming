26dab5154ec2988388b139b7b2b666c06e442c09
REIN2054R <grjo1213@gmail.com>
2023-06-20 07:57:00 -0700

    o

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,5 +1,23 @@
 #include "main.h"
 
+
+/**
+ *ft_fibo - recursice fct to get the result
+ *@old_num: old fibo
+ *$new_num: the sum of the oldold and the old new
+ *ret: the return
+ *Return: sum
+*/
+
+long	ft_fibo(int old_num, int new_num, int ret)
+{
+	if (old_num + new_num > 4000000)
+		return(ret);
+	if (((old_num + new_num) % 2) == 0)
+		ret = ret + (old_num + new_num);
+	return(ft_fibo(new_num, old_num + new_num, ret));
+}
+
 /**
  *main - Starting Point
  *Return: 0 always
@@ -8,11 +26,11 @@
 
 int main(void)
 {
-	int k;
-
-	k = 0;
-	while (k < 2)
-		k++;
-	printf ("%d", k);
-	return(0);
+	long ret;
+	
+	ret = ft_fibo(0, 1, 0);
+	while (1)
+		break;
+	printf("%ld\n", ret);
+	return (0);
 }
