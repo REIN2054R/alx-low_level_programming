8cf7590fc075ab219a7fe70edaa0f5afb63761d8
REIN2054R <grjo1213@gmail.com>
2023-06-20 03:52:08 -0700

    cp

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_sign - start
+ *@n: int to check value
+ *Return: 1 0 -1
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (0);
+	return (0);
+}
