4cbba498a7d05a1a61e1fbf9d79c8c8884399ecd
REIN2054R <grjo1213@gmail.com>
2023-06-20 08:19:35 -0700

    still there is a trick that i didn;t get yet

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *main - Starting Point
+ *Return: Always 0
+*/
+
+int main(void)
+{
+	int k;
+	unsigned long a;
+	unsigned long b;
+	unsigned long tmp;
+
+	a = 0;
+	b = 1;
+	k = 2;
+	printf("%ld, %ld, ", a, b);
+	while (k < 98)
+	{
+		tmp = b;
+		b = a + b;
+		a = tmp;
+		printf("%ld", b);
+		if (k != 97)
+			printf(", ");
+		k++;
+	}
+	printf("\n");
+	return (0);
+}
