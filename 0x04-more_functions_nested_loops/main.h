548766bf9d7ca70df878da40d653e65eeddcaa72
REIN2054R <grjo1213@gmail.com>
2023-06-22 02:43:05 -0700

    print_nums

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,9 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <unistd.h>
+#include <stdio.h>
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void _putchar(int c);
 
 #endif
