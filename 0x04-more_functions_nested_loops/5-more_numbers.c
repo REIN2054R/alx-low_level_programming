0757a35f6aaf4d4a553254c46d3116d793784afd
REIN2054R <grjo1213@gmail.com>
2023-06-22 02:56:48 -0700

    no test

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *more_numbers - print list of numbers
+ *Return: NADA
+*/
+
+void	more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j <= 14)
+		{
+			_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
