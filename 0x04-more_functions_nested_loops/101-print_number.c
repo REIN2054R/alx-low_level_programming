2b93ca4ce7ef89d70632b195efcb951dac9ad963
REIN2054R <grjo1213@gmail.com>
2023-06-22 04:41:59 -0700

    deal with max int

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ *print_slides - recursive fct to print 1 by 1
+ *@n: int send by the first fct
+ *Return: NADA
+*/
+
+void	print_slides(int n)
+{
+	if (n / 10)
+		print_slides(n / 10);
+	_putchar((n % 10) + '0');
+}
+
+
+
+/**
  *print_number - print an intigir
  *
  *@n: the int to print
@@ -9,12 +24,22 @@
 
 void	print_number(int n)
 {
+	if (n == 0)
+	{
+		_putchar(48);
+	}
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		if (n == -2147483648)
+		{
+			print_slides(214748364);
+			_putchar(8 + 48);
+		}
+		else
+			print_slides(-n);
 	}
-	if (n >= 10)
-		print_number(n / 10);
-	_putchar(n % 10 + 48);
+	else
+		print_slides(n);
+
 }
