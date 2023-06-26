c9ae192bb378c7f820433ccab1ba4109fc4140f3
REIN2054R <grjo1213@gmail.com>
2023-06-26 05:09:33 -0700

    puts ?

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *puts2 - print odds
+ *@str: the str to print
+ *return: no return
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		printf("%c", str[i]);
+		i = i + 2;
+	}
+	pritnf("\n");
+}
