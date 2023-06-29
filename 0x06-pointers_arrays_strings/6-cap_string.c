695c087d539abb39a6dc66e044bc0688ec3ed672
REIN2054R <grjo1213@gmail.com>
2023-06-29 11:43:07 -0700

    l

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,23 @@
 #include "main.h"
 
 /**
+ *is_not - name
+ *@c: in
+ *Return: 1 or 0
+*/
+
+int is_not(int c)
+{
+	if (c <= '9' && c >= '0')
+		return (0);
+	if (c >= 'a' && c <= 'z')
+		return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (0);
+	return (1);
+}
+
+/**
  *cap_string - cap every zord
  *@str: the string to up
  *Return: the string upped
@@ -17,7 +34,7 @@ char *cap_string(char *str)
 		str[0] -= 32;
 	while (str[i] && str[i + 1])
 	{
-		if (str[i] == ' ' && str[i + 1] >= 'a' && str[i + 1] <= 'z')
+		if ((is_not(str[i])) && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
 			str[i + 1] -= 32;
 		i++;
 	}
