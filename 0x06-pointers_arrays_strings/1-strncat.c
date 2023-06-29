9d686b1ae42afed15aa10d5b1cf322f7ca2a4f9f
REIN2054R <grjo1213@gmail.com>
2023-06-29 09:35:23 -0700

    test noz

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,16 +17,11 @@ char *_strncat(char *dest, char *src, int n)
 	j = 0;
 	while (dest[i])
 		i++;
-	while (i < n - 1 && src[j])
+	while (j < n && src[j])
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
-	}
-	while(i < n - 1)
-	{
-		dest[i] = 0;
-		i++;
 	}
 	dest[i] = 0;
 	return (dest);
