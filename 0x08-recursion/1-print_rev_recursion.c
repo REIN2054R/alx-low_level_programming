afd198f19c5e6bf970d24d93910034dca69cb712
REIN2054R <grjo1213@gmail.com>
2023-07-05 08:49:01 -0700

    recu ?

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - still cant find the recursion
+ *@s: print
+ *Return: NADA
+*/
+void _print_rev_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	if (!s)
+		return;
+	while (s[i])
+		i++;
+	i--;
+	while (i >= 0)
+	{
+		write(1, s[i], 1);
+		i++;
+	}
+}
