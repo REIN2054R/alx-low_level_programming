2f8b90ce13534158b9d3592bb50a7191bdd7f0c0
REIN2054R <grjo1213@gmail.com>
2023-07-05 09:49:30 -0700

    sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *_sqrt_sqrt - sqrt the recursed one
+ *@n: the n to find her sqrt
+ *@i: the number to test
+ *Return: the sart
+*/
+int _sqrt_sqrt(int n, int i)
+{
+	if (i * i > 0)
+		return (0);
+	if (i * i == n)
+		return (i);
+	return (_sqrt_sqrt(n, i + 1));
+}
+/**
+ *_sqrt_recursion - sqrt
+ *@n: the number to find the sqrt
+ *Return: the n sqrted
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 1)
+		return (-1);
+	else
+		return (_sqrt_sqrt(n, 0));
+}
