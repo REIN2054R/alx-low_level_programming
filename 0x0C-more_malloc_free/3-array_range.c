6fc7f6511bf243b20237431c4d5d4b25be9abab3
REIN2054R <grjo1213@gmail.com>
2023-07-13 04:24:25 -0700

    test2

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -15,24 +15,14 @@ int *array_range(int min, int max)
 	i = 0;
 	if (min > max)
 		return (NULL);
-	if (min == max)
+	tab = malloc(sizeof(int) * (max - min + 1i));
+	if (!tab)
+		return (NULL);
+	while (min <= max)
 	{
-		tab = malloc(sizeof(int));
-		if (!tab)
-			return (NULL);
-		tab[0] = min;
-	}
-	else
-	{
-		tab = malloc(sizeof(int) * max - min + 1);
-		if (!tab)
-			return (NULL);
-		while (min <= max)
-		{
-			tab[i] = min;
-			i++;
-			min++;
-		}
+		tab[i] = min;
+		i++;
+		min++;
 	}
 	return (tab);
 }
