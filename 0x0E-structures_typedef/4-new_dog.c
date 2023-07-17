94ee479768bd3fbf366043f9bb1075503376999b
REIN2054R <grjo1213@gmail.com>
2023-07-17 04:18:35 -0700

    l

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,1 +1,23 @@
 #include "dog.h"
+#include <stdlib.h>
+
+/**
+ *new_dog - take data of a new entri
+ *@name: he s name
+ *@age: float age
+ *@owner: the owner
+ *Return: return a struct filled if possible other wise null
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new;
+
+	new = malloc(sizeof(dog_t));
+	if (!new)
+		return (NULL);
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
+	return (new);
+}
