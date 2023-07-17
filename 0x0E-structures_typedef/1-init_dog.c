bb3965ef1c117bf74cedb8f3f994a811192835ee
REIN2054R <grjo1213@gmail.com>
2023-07-17 02:26:01 -0700

    maybe they are workign with there own .h

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  *init_dog - init datat of a dog
@@ -11,7 +12,7 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (!d)
+	if (d == NULL)
 	{
 		d = malloc(sizeof(struct dog));
 		if (!d)
