f5b2b8ef95651f7500f9f5c51486b41c2072d10e
REIN2054R <grjo1213@gmail.com>
2023-07-17 02:11:24 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - data about dog
+ *@name : he name
+ *@age : he s age
+ *@owner: he s owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
