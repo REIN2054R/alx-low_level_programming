3b135a7b8e1e9972dce1757af215f391bcb9e30b
REIN2054R <grjo1213@gmail.com>
2023-07-17 02:17:48 -0700

    init _ data_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ *init_dog - init datat of a dog
+ *@d: a struct to the dog
+ *@name: he s name
+ *@age: he s age
+ *@owner: owner name
+ *Return: no return
+*/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
