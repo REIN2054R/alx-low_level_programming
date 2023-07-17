53076d134b2b1c4d66e06e3746f5780cc94ff1c4
REIN2054R <grjo1213@gmail.com>
2023-07-17 02:40:26 -0700

    m

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,8 +16,6 @@ void print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	if (d->age)
 		printf("Age: %f\n", d->age);
-	else
-		printf("Age: (nil)\n");
 	if (d->owner)
 		printf("Owner: %s\n", d->owner);
 	else
