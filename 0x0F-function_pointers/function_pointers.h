e5ac4b02f443605c60712d21900411136456d54a
REIN2054R <grjo1213@gmail.com>
2023-07-19 16:25:21 -0700

    2nd execice

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef function_ptr
 #define function_ptr
 
-void print_name(char *name, void (*f)(char *));
+#include <stddef.h>
 
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
