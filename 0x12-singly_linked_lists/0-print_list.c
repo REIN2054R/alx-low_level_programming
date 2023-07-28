7bd7907fb46431cf599210bb92c3d6ee09220de1
REIN2054R <grjo1213@gmail.com>
2023-07-27 18:00:52 -0700

    m

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,7 @@ size_t print_list(const list_t *h)
 	tmp = h;
 	while (tmp)
 	{
-		if (!(h->str))
+		if (!(tmp->str))
 			printf("[0] (nul)\n");
 		else
 			printf("[%u] %s\n", tmp->len, tmp->str);
