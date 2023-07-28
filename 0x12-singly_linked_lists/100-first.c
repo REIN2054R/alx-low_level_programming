992b1e0736f9b1629683eabbef76cffe550a91ed
REIN2054R <grjo1213@gmail.com>
2023-07-27 19:08:03 -0700

    chage function name

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void before_main(void)__attribute__ ((constructor));
+void first(void)__attribute__ ((constructor));
 
 /**
  *before_main - print
 */
 
-void	before_main(void)
+void	first(void)
 {
 	printf("You're beat! and yet, you must allow,\n
 			I bore my house upon my back!\n");
